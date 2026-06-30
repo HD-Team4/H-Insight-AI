bb8106f26d6a6017776510d04441acdfff06a1fc
chaeyeon Kong <66231912+chaeyeonKong@users.noreply.github.com>
2026-06-30 16:12:47 +0900

    feat: 전체 상품 조회 기능 구현

--- src/main/java/com/hinsight/product/controller/ProductController.java
+++ src/main/java/com/hinsight/product/controller/ProductController.java
@@ -1,9 +1,25 @@
 package com.hinsight.product.controller;
 
+import com.hinsight.product.service.ProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/products")
 public class ProductController {
+
+    private final ProductService productService;
+
+    @GetMapping
+    public String productList(Model model) {
+        model.addAttribute("products", productService.getAllProducts());
+        return "customer/product/list";
+    }
+
+
+
 }
--- src/main/java/com/hinsight/product/service/ProductService.java
+++ src/main/java/com/hinsight/product/service/ProductService.java
@@ -1,7 +1,20 @@
 package com.hinsight.product.service;
 
+import com.hinsight.product.dao.ProductDao;
+import com.hinsight.product.model.vo.Product;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class ProductService {
+
+    private final ProductDao productDao;
+
+    public List<Product> getAllProducts() {
+        return productDao.getAllProducts();
+    }
+
 }
